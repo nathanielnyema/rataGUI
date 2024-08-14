e9f7b000f2545e5da627a3bf7220118e90e5f79f
Brian Hu <brainhu42@gmail.com>
2024-08-14 10:13:52 -0700

    switches conda env for setup2

--- rataGUI.bat
+++ rataGUI.bat
@@ -1,4 +1,4 @@
 @echo on
 call C:\Users\Siapas\anaconda3\Scripts\activate.bat
 call activate fc_rataGUI
-C:\Users\Siapas\anaconda3\envs\fc_rataGUI\python.exe -m "rataGUI.main"
+C:\Users\Siapas\anaconda3\envs\rataGUI\python.exe -m "rataGUI.main"
